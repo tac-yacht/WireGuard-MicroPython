b2a35da3eedd9eae5500e6d050415252415dd985
tac-yacht <tac-yacht@users.noreply.github.com>
2025-05-05 16:54:44 +0900

    experimental initial version

---
+++ WireGuard-micropython.cmake
@@ -0,0 +1,26 @@
+# Create an INTERFACE library for our CPP module.
+add_library(usermod_wireguard INTERFACE)
+
+# Add our source files to the library.
+target_sources(usermod_wireguard INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/crypto/refc/blake2s.c
+    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/crypto/refc/chacha20.c
+    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/crypto/refc/chacha20poly1305.c
+    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/crypto/refc/poly1305-donna.c
+    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/crypto/refc/x25519.c
+    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/crypto.c
+    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/wireguard.c
+    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/WireGuard.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/wireguardif.c
+    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/wireguard-platform.c
+    ${CMAKE_CURRENT_LIST_DIR}/wireguard-mp-wrapper.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/wireguard-mp-module.c
+)
+
+# Add the current directory as an include directory.
+target_include_directories(usermod_wireguard INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+)
+
+# Link our INTERFACE library to the usermod target.
+target_link_libraries(usermod INTERFACE usermod_wireguard)
---
+++ wireguard-mp-wrapper.h
@@ -0,0 +1,34 @@
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "py/runtime.h"
+#include "py/obj.h"
+#include "py/objstr.h"
+
+#define MP_OBJ_NEW_STR(str) mp_obj_new_str(str, sizeof(str) - 1)
+
+mp_obj_t begin(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
+static const MP_DEFINE_STR_OBJ(subnet_default, "255.255.255.255");
+static const MP_DEFINE_STR_OBJ(gateway_default, "0.0.0.0");
+const mp_arg_t begin_allowed_args[] = {
+	{MP_QSTR_local_ip, MP_ARG_OBJ|MP_ARG_REQUIRED},
+	{MP_QSTR_subnet, MP_ARG_OBJ, {.u_rom_obj = MP_ROM_PTR(&subnet_default)}},
+	{MP_QSTR_gateway, MP_ARG_OBJ, {.u_rom_obj = MP_ROM_PTR(&gateway_default)}},
+	{MP_QSTR_private_key, MP_ARG_OBJ|MP_ARG_REQUIRED},
+	{MP_QSTR_remote_peer_address, MP_ARG_OBJ|MP_ARG_REQUIRED},
+	{MP_QSTR_remote_peer_public_key, MP_ARG_OBJ|MP_ARG_REQUIRED},
+	{MP_QSTR_remote_peer_port, MP_ARG_INT|MP_ARG_REQUIRED},
+	{ MP_QSTR_dry_run, MP_ARG_BOOL, {.u_bool = false} },
+};
+
+/*None*/
+mp_obj_t end();
+
+/*bool*/
+mp_obj_t is_initialized();
+
+#ifdef __cplusplus
+}
+#endif

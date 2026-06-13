5f2d3d735b30cbd65b9a65caf260629af9f8e5dd
tac-yacht <tac-yacht@users.noreply.github.com>
2026-06-13 23:55:45 +0900

    コンパイル方法変更に追従

--- WireGuard-micropython.cmake
+++ WireGuard-micropython.cmake
@@ -1,8 +1,8 @@
 # Create an INTERFACE library for our CPP module.
-add_library(usermod_wireguard OBJECT)
+add_library(usermod_wireguard INTERFACE)
 
 # Add our source files to the library.
-target_sources(usermod_wireguard PRIVATE
+target_sources(usermod_wireguard INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/crypto/refc/blake2s.c
     ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/crypto/refc/chacha20.c
     ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/crypto/refc/chacha20poly1305.c
@@ -19,11 +19,11 @@ target_sources(usermod_wireguard PRIVATE
 )
 
 # Add the current directory as an include directory.
-target_include_directories(usermod_wireguard PUBLIC
+target_include_directories(usermod_wireguard INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
-target_link_libraries(usermod_wireguard PRIVATE
+target_link_libraries(usermod_wireguard INTERFACE
     idf::esp_system
     idf::esp_netif
     idf::lwip
